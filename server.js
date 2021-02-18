b6ba5fe09caac448b7c377626a0dde008adb9315
JUAN ZAPATA <jzapata@overactive.com>
2021-02-18 10:18:29 -0300

    corsOptions

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "https://rosario12pauta.herokuapp.com"
 };
 
 app.use(cors(corsOptions));
